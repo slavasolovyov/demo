96b9bc52ff51d86d7a67828547fb39f2ca1697d3
slavasolovyov <32089268+slavasolovyov@users.noreply.github.com>
2022-09-29 13:17:23 +0300

    test

--- src/main/java/org/demo/config/TestConfiguration.java
+++ src/main/java/org/demo/config/TestConfiguration.java
@@ -4,7 +4,7 @@ import io.github.cdimascio.dotenv.Dotenv;
 
 public class TestConfiguration {
     public static String BASE_URL = "https://selenide.org";
-    public static String SELENOID_URL = System.getProperty("SELENOID_URL", "http://host.docker.internal:4444/wd/hub");
+    public static String SELENOID_URL = System.getProperty("SELENOID_URL", "http://localhost:4444/wd/hub");
     public static boolean USE_LOCAL_BROWSER = Boolean.parseBoolean(System.getProperty("USE_LOCAL_BROWSER", "true"));
 
 
