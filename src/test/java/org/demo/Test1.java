a3fa5079eda605e70efcf32b0d4ec9b4c4c82067
viacheslavsolovyov <killV.11>
2022-10-11 16:12:43 +0300

    comment cdp

--- src/test/java/org/demo/Test1.java
+++ src/test/java/org/demo/Test1.java
@@ -1,28 +1,18 @@
 package org.demo;
 
-import static java.lang.String.format;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.demo.Pages.Pages.Entrypoint.AUTH_PAGE;
 import static org.demo.Pages.Pages.Entrypoint.SPORTS_PREMATCH;
 import static org.demo.Pages.Pages.openPage;
-import static org.demo.config.TestConfiguration.MOBILE;
-import static org.demo.config.TestConfiguration.SELENOID_URL;
 
-import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.WebDriverRunner;
-import io.github.sskorol.cdt.protocol.commands.Emulation;
-import io.github.sskorol.cdt.services.ChromeDevToolsService;
 import io.qameta.allure.Description;
 import io.qameta.allure.Issue;
 import io.qameta.allure.Owner;
 import io.qameta.allure.TmsLink;
 import lombok.SneakyThrows;
-import org.demo.Pages.user.PrematchPage;
-import org.demo.config.TestConfiguration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
-import org.openqa.selenium.remote.RemoteWebDriver;
+
 
 import java.util.List;
 
