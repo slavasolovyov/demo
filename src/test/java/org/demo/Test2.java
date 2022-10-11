1e3a45dafc7002082967a7e80b04a5b494f022be
viacheslavsolovyov <killV.11>
2022-10-11 17:34:27 +0300

    add parallel

--- src/test/java/org/demo/Test2.java
+++ src/test/java/org/demo/Test2.java
@@ -1,9 +1,5 @@
 package org.demo;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.demo.Pages.Pages.Entrypoint.SPORTS_PREMATCH;
-import static org.demo.Pages.Pages.openPage;
-
 import io.qameta.allure.Description;
 import io.qameta.allure.Issue;
 import io.qameta.allure.Owner;
@@ -15,12 +11,15 @@ import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
 
-
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.demo.Pages.Pages.Entrypoint.SPORTS_PREMATCH;
+import static org.demo.Pages.Pages.openPage;
+
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@Execution(ExecutionMode.CONCURRENT)
-public class Test1 extends BaseTest{
+//@Execution(ExecutionMode.CONCURRENT)
+public class Test2 extends BaseTest{
 
     @BeforeAll
     @SneakyThrows
@@ -44,7 +43,7 @@ public class Test1 extends BaseTest{
     @Owner("vsoloviov@gmail.com")
     @Issue("JIRA-001")
     @TmsLink("TMS-001")
-    void testPosetive(){
+    void testPosetive_1(){
         List<String> leftMenuItems = openPage(SPORTS_PREMATCH).getNavigationSliderItems();
         assertThat(leftMenuItems.size()).isGreaterThan(0);
     }
@@ -54,7 +53,7 @@ public class Test1 extends BaseTest{
     @Owner("vsoloviov@gmail.com")
     @Issue("JIRA-001")
     @TmsLink("TMS-001")
-    void testPosetive2(){
+    void testPosetive_2(){
         List<String> leftMenuItems = openPage(SPORTS_PREMATCH).getNavigationSliderItems();
         assertThat(leftMenuItems.size()).isGreaterThan(0);
     }
@@ -64,7 +63,7 @@ public class Test1 extends BaseTest{
     @Owner("vsoloviov@gmail.com")
     @Issue("JIRA-001")
     @TmsLink("TMS-001")
-    void testPosetive3(){
+    void testPosetive_3(){
         List<String> leftMenuItems = openPage(SPORTS_PREMATCH).getNavigationSliderItems();
         assertThat(leftMenuItems.size()).isGreaterThan(0);
     }
